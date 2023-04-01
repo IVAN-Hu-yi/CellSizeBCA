a7d1704954db431a00cdb1f4d8a4e9b1faff043a
Yingcai Hu <ivann1329@outlook.com>
2023-04-01 21:01:26 +0100

    add resource preferences

--- Code/initialisation.py
+++ Code/initialisation.py
@@ -3,35 +3,59 @@ from parametres import paras
 
 
 # Initial Conditions
+seed = 100
 
 def int_R(M, R): 
     '''
         Resource Content at t0
     '''
-    return np.random.normal(R, 0.5, M).T
+    np.random.seed(seed)
+    return np.random.normal(R, 0.5, M).reshape(M, 1)
 
 def int_C(N, w):
     '''
         Biomass Content drawn from beta distribution at t0
     '''
-    return 10 ** np.random.beta(1, w, N).T
+    np.random.seed(seed)
+    return 10 ** np.random.beta(1, w, N).reshape(N, 1)
 
 # define parametres
+
+def int_preferences(N, M, mu_c, c0):
+    
+    '''Guassian sampling of preferences, assume all are generalilsts
+
+    Returns:
+        np.array: N*M matries
+    '''
+
+    p = np.full((N*M), c0)
+    for i in range(N):
+        np.random.seed(i) # ensure for each assembly, each species same preferences
+        number = int(mu_c*M) if int(mu_c*M) > 0 else 1
+        idx = np.random.randint(0, M-1, number) # select favored resoruces
+        np.random.seed(i)
+        p[i, :][idx] = np.random.normal(mu_c, 0.001, number) # assign values
+        p[i, :] = p[i, :]/np.sum(p[i, :]) # normalised to 1
+    return p
+
 
 def int_l(M, l, same=True):
     '''
      return leakage
     '''
-    if same: return np.array([l]*M).T
-    else: return np.random.normal(l, 0.005, M).T 
+    np.random.seed(seed)
+    if same: return np.array([l]*M).reshape(M, 1)
+    else: return np.random.normal(l, 0.005, M).reshape(M, 1)
 
 def int_rho(M, rho):
     '''
     define external resource supply
     '''
-    return np.array([rho]*M).T
+    np.random.seed(seed)
+    return np.array([rho]*M).reshape(M, 1)
 
-def int_vmax(M, N, vmax):
+def int_vmax(N, M, vmax):
     '''generate maximum uptake
 
     Args:
@@ -42,9 +66,9 @@ def int_vmax(M, N, vmax):
     Returns:
         N*M matrix: _description_
     '''
-
+    np.random.seed(seed)
     return np.random.normal(vmax, 0.01, (N, M))
 
 def int_mt(N, m):
-
-    return np.random.normal(m, 0.01, N)
+    np.random.seed(seed)
+    return np.random.normal(m, 0.01, (N, 1))
