b21cfcd48e72e808f1e2bc2d302f962f044ba1e6
Yingcai Hu <ivann1329@outlook.com>
2023-04-02 16:19:26 +0100

    setting seeds, modify vmax, preferences initialisation

--- Code/initialisation.py
+++ Code/initialisation.py
@@ -3,45 +3,47 @@ from parametres import paras
 
 
 # Initial Conditions
-seed = 100
+seed = 100 # set seed to ensure reproducible results in each experiment
+# assemblenum: int -> set random state for each individual assembly process
 
-def int_R(M, R): 
+def int_R(M, R, assemblenum): 
     '''
         Resource Content at t0
     '''
-    np.random.seed(seed)
+    np.random.seed(seed+assemblenum)
     return np.random.normal(R, 5, M).reshape(M, 1)
 
-def int_C(N, w):
+def int_C(N, w, assemblenum):
     '''
         Biomass Content drawn from beta distribution at t0
     '''
-    np.random.seed(seed)
+    np.random.seed(seed+assemblenum)
     return 10 ** np.random.beta(1, w, N).reshape(N, 1)
 
 # define parametres
 
-def int_preferences(N, M, mu_c, c0):
+def int_preferences(N, M, mu_c, assemblenum):
     
     '''Guassian sampling of preferences, assume all are generalilsts
 
     Returns:
         np.array: N*M matries
+        number: number of preferred resources
     '''
 
-    p = np.full((N*M), c0).reshape(N, M)
-    print(p)
+    p = np.full((N*M), 1/(M*10)).reshape(N, M)
+    number = int(mu_c*M) if int(mu_c*M) > 0 else 1
+
     for i in range(N):
-        np.random.seed(i) # ensure for each assembly, each species same preferences
-        number = int(mu_c*M) if int(mu_c*M) > 0 else 1
+        np.random.seed(i*assemblenum*i) # ensure for each experiment, each species same preferences
         idx = np.random.randint(0, M-1, number) # select favored resoruces
         temp = p[i, :]
-        np.random.seed(i)
-        temp[idx] = np.random.normal(mu_c, 0.001, number) # assign values
+        np.random.seed(i*assemblenum*i)
+        temp[idx] = np.random.normal(1/number, 0.001, number) # assign values
         p[i, :] = p[i, :]/np.sum(p[i, :]) # normalised to 1
-    return p
+    return p, number
 
-def int_conversion(M, Dbase):
+def int_conversion(M, Dbase, assemblenum):
 
     '''guassian sampling of conversion around Dbase
 
@@ -53,40 +55,53 @@ def int_conversion(M, Dbase):
         np.array: shape input_resource * converted resource 
     '''
 
-    np.random.seed(seed)
+    np.random.seed(seed+assemblenum)
     D = np.random.normal(Dbase, 0.1, (M, M)).reshape(M, M) # sample conversion
     np.fill_diagonal(D, 0) # assume all metabolites convert to different form
     return D/np.sum(D, axis=0)[np.newaxis, :] # column-wise normalisation
     
-def int_l(M, l, same=True):
+def int_l(M, l, assemblenum, same=True):
     '''
      return leakage
     '''
-    np.random.seed(seed)
+    np.random.seed(seed+assemblenum)
     if same: return np.array([l]*M).reshape(M, 1)
     else: return np.random.normal(l, 0.005, M).reshape(M, 1)
 
-def int_rho(M, rho):
+def int_rho(M, rho, assemblenum):
     '''
     define external resource supply
     '''
-    np.random.seed(seed)
+    np.random.seed(seed+assemblenum)
     return np.array([rho]*M).reshape(M, 1)
 
-def int_vmax(N, M, vmax):
-    '''generate maximum uptake
+def int_vmax(N, M, v_max_base, p, number, assemblenum):
+
+    '''initialised maximum uptake of each resource 
 
     Args:
-        M (int): int
-        N (int): _description_
-        vmax (int): _description_
+        v_max_base: mean vmax for uptake
+        p: preferences for identify preferred resource
+        number: number of preferred resources
 
     Returns:
-        N*M matrix: _description_
+        np.array : N by M matrix
     '''
-    np.random.seed(seed)
-    return np.random.normal(vmax, 0.01, (N, M))
 
-def int_mt(N, m):
-    np.random.seed(seed)
-    return np.random.normal(m, 0.01, (N, 1))
+    vmax = np.zeros((N, M))*0.1 # for non-favored 0.1 max uptake
+    for i in range(N):
+        temp_p = p[i, :] # identify preferred resource
+        temp_vmax = vmax[i, :]
+        sorted = temp_p[::-1].sort()
+        val = sorted[number-1] # least preferred resource among the preferred
+        np.random.seed(i+40+assemblenum)
+        temp_vmax[temp_p>=val] = np.random.normal(v_max_base, 1.5, len(temp_vmax[temp_p>0.01])) # define max uptake
+        vmax[i, :] = temp_vmax # update temp_vmax
+    
+    return vmax
+
+    
+
+def int_mt(N, m, assemblenum):
+    np.random.seed(seed+assemblenum)
+    return np.random.normal(m, 0.01, (N, 1)).reshape(N, 1)
--- Code/unitilities.py
+++ Code/unitilities.py
@@ -1,5 +1,30 @@
-# Caculate helper functions including ploting funcs
+# Helper functions
 
-## intermediate calcualtions
+## intermediate values calcualtions
 
-def 
+import numpy as np
+
+def vin(p, R, Rhalf, vmax=None, type=2):
+    '''Calcualte Vin for each species
+
+    Args:
+        p (N by M matrix): preference 
+        R (M by 1 matrix): resource concentration
+        Rhalf ( float ): half-velocity constant
+        vmax (N by M matrix, optional): maximum uptake rate. Defaults to None.
+        type (int, optional): Type I or Type II functional response. Defaults to 2.
+
+    Returns:
+       N by 1 vector: Vin for each species
+    '''
+    # calculate uptake
+    uptake = p*R.T[:, np.newaxis]
+    uptake = uptake.reshape(p.shape)
+
+    if type==2: # Monod function
+        if vmax is None:
+            print('Vmax required for monod function')
+        else:
+            return vmax*((uptake)/(Rhalf + uptake))
+    elif type==1: # linear
+        return uptake
