52b1576c696c8f0256f7ab75640cab315dca1026
Yingcai Hu <ivann1329@outlook.com>
2023-04-03 10:39:14 +0100

    fix indention

--- Code/parametres.py
+++ Code/parametres.py
@@ -21,10 +21,10 @@ class Paras:
 
         ####### Model Paras fixed
 
-    self.R_half = 10 # coefficient in sigma func
-    self.mu = 1 # intrinsic growth rate  mass^-1
-    self.km = 1 # individual per unit mass mass^-1
-    self.rho_base = 20
+        self.R_half = 10 # coefficient in sigma func
+        self.mu = 1 # intrinsic growth rate  mass^-1
+        self.km = 1 # individual per unit mass mass^-1
+        self.rho_base = 20
 
     ####### Scaling Paras
 
@@ -35,8 +35,8 @@ class Paras:
         self.gamma = 0.86 # size-scaling exponent for outflow
 
     ####### initialisation
-    self.R = 50 # initial resource mass
-    self.w = 2 # one parameter distribution 1 or 2 B(1, w)
+        self.R = 50 # initial resource mass
+        self.w = 2 # one parameter distribution 1 or 2 B(1, w)
 
         ####### relevant paras in consumer preferences (similar to Marsland 2019)
         self.mu_c = 0.1 # proportion of favor resources types
@@ -45,14 +45,14 @@ class Paras:
         ####### relevant paras in conversion efficiency
         self.Dbase = 0.5 # base efficiency for all resource-resource pair 
 
-def paras(self, Ci, Ri, l, rho, p, vmax, m, D):
-    
-    ####### Model Paras defined
-    self.C = Ci # initial biomass for each species N*1 array
-    self.R = Ri # initial resource content for each resource type M*1 array
-    self.l = l # leakage fraction for each resource M*1 matrix
-    self.rho = rho # external resource supply M*1 array
-    self.p = p # resource preferences N*M matrix
-    self.v_in_max = vmax # max uptake/sigma func N*M matrix
-    self.m = m # maintainence N*1 array
-    self.D = D # conversion effciency
+    def paras(self, Ci, Ri, l, rho, p, vmax, m, D):
+
+        ####### Model Paras defined
+        self.C = Ci # initial biomass for each species N*1 array
+        self.R = Ri # initial resource content for each resource type M*1 array
+        self.l = l # leakage fraction for each resource M*1 matrix
+        self.rho = rho # external resource supply M*1 array
+        self.p = p # resource preferences N*M matrix
+        self.v_in_max = vmax # max uptake/sigma func N*M matrix
+        self.m = m # maintainence N*1 array
+        self.D = D # conversion effciency
