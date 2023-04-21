7dfbfd95e9d5dad01f13c50730dcc1b4f7bedc83
Yingcai Hu <ivann1329@outlook.com>
2023-04-21 01:04:43 +0100

    change back to beta = 0.75

--- Code/parametres.py
+++ Code/parametres.py
@@ -27,7 +27,7 @@ class Paras:
         self.E0 = 1e-03 # Normalisation constant for outflow
         self.alpha = - 0.25 # size-scaling exponent for inflow 
         self.gamma = 0.86 # size-scaling exponent for outflow
-        self.beta = -0.25 # maintainence
+        self.beta = 0.75 # maintainence
 
         ####### initialisation
         self.R0 = 10 # initial resource mass
